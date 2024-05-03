e3c9e322406acc7abc727d9634a3f72945a29aa2
lisafuhrmann <lisa_marie.fuhrmann@smail.th-koeln.de>
2024-05-03 14:40:28 +0200

    keyword search

--- json-content/articlelist.js
+++ json-content/articlelist.js
@@ -1,6 +1,8 @@
 const articleListURL =
   "https://gist.githubusercontent.com/vschaefer/8d26be957bbc8607f60da5dd1b251a78/raw/49ddd7c2636fb722912d91107aff55c79ddf05a8/articles.json";
 
+let currentData = null;
+
 document.addEventListener("DOMContentLoaded", function () {
   initArticleList();
 });
@@ -10,7 +12,29 @@ function initArticleList() {
   responsePromise.then(function (response) {
     const dataPromise = response.json();
     dataPromise.then(function (data) {
+      currentData = data;
       renderArticleList(data.articles);
+      initFilters();
+    });
+  });
+}
+
+function filterArticles(filterValue) {
+  const filteredArticles = currentData.articles.filter(function (article) {
+    return article.tags.keywords.includes(filterValue);
+  });
+  return filteredArticles;
+}
+
+function initFilters() {
+  const filterButtons = document.querySelectorAll(
+    '[data-js-category="keywords"] [data-js-filter]'
+  );
+  filterButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      const filter = event.currentTarget.getAttribute("data-js-filter");
+      const filteredArticles = filterArticles(filter);
+      renderArticleList(filteredArticles);
     });
   });
 }
